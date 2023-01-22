572f9ab2c33d760aa387252da9eefebb209696fb
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-01-22 11:44:14 +0500

    added the POST login API test

---
+++ cypress/e2e/Direct-Apply/3-api-testing/login-api.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('POST API Login Request', () => {
+
+const core_url = "https://sandbox.cialfo.sg/v3/sessions?access_token=Zx2EE58K0tyzL8V4Xh7GLYYhQzHcz7Q05jrRXAcfw1&locale=en-US&platform=web"
+
+it('POST request', () => {
+cy.request({
+    method: 'POST',
+    url: core_url,
+    headers: {
+        'Content-Type': 'application/json',
+        'access_token': 'Zx2EE58K0tyzL8V4Xh7GLYYhQzHcz7Q05jrRXAcfw1',
+        'locale': 'eng-US',
+        'platform': 'web'
+    },
+    body: {
+        email: 'tayyab.javed+student150@cialfo.com.sg',
+        password: 'Test1234'
+    }
+}).then((response) => {
+    expect(response.status).to.eq(200);
+    expect(response.headers).to.have.property('content-type', 'application/json; charset=utf-8');
+});
+})
+})
