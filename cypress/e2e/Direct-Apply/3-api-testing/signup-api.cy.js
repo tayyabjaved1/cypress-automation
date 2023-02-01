8b7c370f7bff345e9fd4edf67a5d463b70d1af08
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-02-01 12:25:15 +0500

    added response body  message

--- cypress/e2e/Direct-Apply/3-api-testing/signup-api.cy.js
+++ cypress/e2e/Direct-Apply/3-api-testing/signup-api.cy.js
@@ -34,23 +34,23 @@ describe('Signup Tests', () => {
     }
     }).then(response => {
       expect(response.status).to.eq(expectedStatus)
-     // expect(response.body).to.have.property('message', expectedMessage)
+      expect(response.body.status).to.have.property('message', expectedMessage)
      //expect(response.body).to.have.property('email', expectedValue)
 
     })
   }
 
   it('Verify the response when the student is signed up successfully', () => {
-    signup('Tayyab', randomString(), randomEmail, 'Test1234', 'M', 200)
+    signup('Tayyab', randomString(), randomEmail, 'Test1234', 'M', 200, 'OK')
     //cy.wait(15000)
   })
 
   it('Should return 422 status code for missing required information including "Password", "Email", "LastName" ', () => {
-    signup('Tayyab', '', '', '', 'M', 422)
+    signup('Tayyab', '', '', '', 'M', 422, 'Unprocessable Entity')
     cy.wait(40000)
   })
 
   it('Verify the response if user tries to signup up with existing email', () => {
-    signup('Tayyab', randomString(), 'tayyab.javed+student150@cialfo.com.sg', 'Random1234', 'M', 422)
+    signup('Tayyab', randomString(), 'tayyab.javed+student150@cialfo.com.sg', 'Random1234', 'M', 422, 'Unprocessable Entity')
   })
 })
