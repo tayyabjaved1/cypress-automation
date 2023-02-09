93fa19a54357d785832824003eaf1ec7ff389aa4
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-02-09 09:47:22 +0500

    GET API adjusted code alignment

--- cypress/e2e/Direct-Apply/3-api-testing/get-api.cy.js
+++ cypress/e2e/Direct-Apply/3-api-testing/get-api.cy.js
@@ -6,19 +6,19 @@ cy.requestHeader =  {
     'User-Agent': 'wTRGdmtTaDUzdCDD',
     'platform': 'web'
   }
-  const programs_types_url = 'https://sandbox.cialfo.sg/v3/schools/2000076/direct_apply_program_types'
-    describe('Get Programs Types', () => {
-      it('Getting the Direct Apply Programs types', () => {
-        cy.request({
-          method: 'GET',
-          url: programs_types_url,
-          headers: cy.requestHeader,
-          failOnStatusCode: false
-        }).then( response => {
-            expect(response.status).to.eq(200)
-            expect(response.body.status.details.direct_apply_program_types[0]).to.have.property('id', 1)
-            expect(response.body.status.details.direct_apply_program_types[0]).to.have.property('title', 'Pre-Sessional English')
-            expect(response.body.status.details.direct_apply_program_types[1]).to.have.property('title', 'Direct Admission')
-        })
-      })
+const programs_types_url = 'https://sandbox.cialfo.sg/v3/schools/2000076/direct_apply_program_types'
+describe('Get Programs Types', () => {
+  it('Getting the Direct Apply Programs types', () => {
+    cy.request({
+      method: 'GET',
+      url: programs_types_url,
+      headers: cy.requestHeader,
+      failOnStatusCode: false
+    }).then( response => {
+      expect(response.status).to.eq(200)
+      expect(response.body.status.details.direct_apply_program_types[0]).to.have.property('id', 1)
+      expect(response.body.status.details.direct_apply_program_types[0]).to.have.property('title', 'Pre-Sessional English')
+      expect(response.body.status.details.direct_apply_program_types[1]).to.have.property('title', 'Direct Admission')
     })
+  })
+})
