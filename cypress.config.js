17dec182dccf6f3070d4f07c15f4999e4d1f676c
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-04-06 08:58:45 +0500

    added cross origin url handling tests in other window

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vc6i9w',
   e2e: {
+    //experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
