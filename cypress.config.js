96451c0414a99ba68faffc8999d20c72383529d3
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-04-07 14:28:55 +0500

    installed & configured mochawesome reporting plugin

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vc6i9w',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Automation-Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      reportFilename: "[status]_[datetime]-[name]-report",
+      code: false,
+      autoOpen: true,
+      overwrite: false
+    },
     //experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://companion-test.cialfo.sg/',
     viewportWidth: 1536,
