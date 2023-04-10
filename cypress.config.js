173bbd19dc0f30444b565e529c69bdbecc21ad74
tayyabjaved1 <tayyabjaved1@outlook.com>
2023-04-10 17:24:44 +0500

    Integrated AIO Test Reporter with Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,45 @@
 const { defineConfig } = require("cypress");
+const { registerAIOTestsPlugin } = require('cypress-aiotests-reporter/src')
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   projectId: 'vc6i9w',
-  e2e: {
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'Automation-Report',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
-      reportFilename: "[status]_[datetime]-[name]-report",
-      code: false,
-      autoOpen: true,
-      overwrite: false
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Automation-Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportFilename: "[status]_[datetime]-[name]-report",
+    code: false,
+    autoOpen: false,
+    overwrite: false,
+    mochaFile: 'results/my-test-output.xml'
+  },
+  env: {
+    "aioTests": {
+      "enableReporting": true,
+      "cloud": { //Replace with server authentication if using Jira Server
+        "apiKey": "OTM2ZWM3YmMtNGZhZi00OWFkLTg0ZDAtMzM0NzZkMDk1Mjcx"
+      },
+      "jiraProjectId": "10051", // Jira Project ID for Godspeed is 10051 //
+      "cycleDetails": {
+        "createNewCycle": false,
+        "cycleName": "Cypress Test Cyle Run on AIO",
+        "cycleKey": "GOD-CY-89"
+      },
+      "addNewRun": true,
+      "addAttachmentToFailedCases": true,
+      "createNewRunForRetries": true
     },
+  },
+  e2e: {
     //experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      registerAIOTestsPlugin(on,config);
     },
     baseUrl: 'https://companion-test.cialfo.sg/',
     viewportWidth: 1536,
